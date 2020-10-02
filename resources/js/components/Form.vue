2241007611c2065b9808b424b70427b15e63602e
stepanozhigov <stepan.ozhigov@gmail.com>
2020-10-02 17:45:42 +0200

    metrics

--- resources/js/components/Form.vue
+++ resources/js/components/Form.vue
@@ -1,30 +1,38 @@
 <template>
-    <form class="form-template" @submit.prevent="submitForm">
-        <div class="flex flex-row">
-            <vue-country-code @onSelect="onCountrySelect" />
-            <masked-input
-                type="tel"
-                autocomplete="off"
-                placeholder="Ваш телефон*"
-                class="w-full ml-4 pl-4"
-                v-model="$v.phone.$model"
-                :mask="{
-                    pattern: '(V11) 111-11-11',
-                    formatCharacters: {
-                        V: {
-                            validate: char => /[0-9]/.test(char)
-                        }
-                    }
-                }"
-                @focus.native="(isValid = true), (onFocus = true)"
-                @blur.native="onFocus = false"
-            />
-        </div>
+  <form
+    class="form-template"
+    :class="{
+      'type-form': type == 'form',
+      'type-callback': type == 'callback',
+    }"
+    @submit.prevent="submitForm"
+  >
+    <div class="flex flex-row">
+      <vue-country-code @onSelect="onCountrySelect" />
+      <masked-input
+        type="tel"
+        autocomplete="off"
+        placeholder="Ваш телефон*"
+        class="w-full ml-4 pl-4"
+        v-model="$v.phone.$model"
+        :mask="{
+          pattern: '(V11) 111-11-11',
+          formatCharacters: {
+            V: {
+              validate: (char) => /[0-9]/.test(char),
+            },
+          },
+        }"
+        @focus.native="(isValid = true), (onFocus = true)"
+        @blur.native="onFocus = false"
+      />
+    </div>
 
-        <button class="button-pulse">
-            <span>Получить консультацию и рассчёт</span>
-        </button>
-    </form>
+    <button class="button-pulse">
+      <span v-if="type == 'form'">Получить консультацию и рассчёт</span>
+      <span v-if="type == 'callback'">Заказать звонок</span>
+    </button>
+  </form>
 </template>
 
 <script>
@@ -36,62 +44,76 @@ import { mapActions, mapGetters } from "vuex";
 //валидация телефона по регулярному вырожению
 ///^(\+)?(\(\d{2,3}\) ?\d|\d)(([ \-]?\d)|( ?\(\d{2,3}\) ?)){5,12}\d$/
 const phoneValidat = helpers.regex(
-    "phoneValidat",
-    /^(\+)?(\(\d{2,3}\) ?\d|\d)(([ \-]?\d)|( ?\(\d{2,3}\) ?)){5,12}\d$/
+  "phoneValidat",
+  /^(\+)?(\(\d{2,3}\) ?\d|\d)(([ \-]?\d)|( ?\(\d{2,3}\) ?)){5,12}\d$/
 );
 
 export default {
-    data: () => ({
-        phone: "",
-        url: "lmr.vskidke.ru",
-        dialCode: "",
-        isValid: true,
-        onFocus: false
-    }),
-    components: { MaskedInput },
-    mounted() {
-        //console.log("Component mounted.");
+  data: () => ({
+    phone: "",
+    url: "lmr.vskidke.ru",
+    dialCode: "",
+    isValid: true,
+    onFocus: false,
+  }),
+  components: { MaskedInput },
+  mounted() {
+    //console.log("Component mounted.");
+  },
+  computed: {
+    ...mapGetters(["isModal", "isSuccess"]),
+    phoneNumber: function () {
+      return "+" + this.dialCode + " " + this.phone;
     },
-    computed: {
-        ...mapGetters(["isModal", "isSuccess"]),
-        phoneNumber: function() {
-            return "+" + this.dialCode + " " + this.phone;
-        }
+  },
+  props: {
+    type: {
+      type: String,
+      default: "none",
     },
-    methods: {
-        ...mapActions(["setModal", "unsetModal", "setSuccess", "unsetSuccess"]),
-        submitForm() {
-            if (this.$v.phone.$invalid) {
-                this.isValid = false;
-            } else {
-                this.isValid = true;
-                //fbq("track", "Lead");
-                axios
-                    .post("api/lead", {
-                        phone: this.phoneNumber,
-                        url: this.url
-                    })
-                    .then(response => {
-                        console.log(response);
+  },
+  methods: {
+    ...mapActions(["setModal", "unsetModal", "setSuccess", "unsetSuccess"]),
+    submitForm() {
+      if (this.$v.phone.$invalid) {
+        this.isValid = false;
+      } else {
+        this.isValid = true;
+        //fbq("track", "Lead");
+        axios
+          .post("api/lead", {
+            phone: this.phoneNumber,
+            url: this.url,
+          })
+          .then((response) => {
+            console.log(response);
 
-                        this.phone = "";
-                        this.setSuccess();
-                        this.setModal();
-                    });
-                // this.phone = "";
-                // this.setSuccess();
-                // this.setModal();
+            if (type == "form") {
+              ym(62231704, "reachGoal", "leadmagnit-form-open-account");
+              ga("send", "event", "leadmagnit-forms-accoint-in-KZ", "send");
+            } else if (type == "callback") {
+              ym(62231704, "reachGoal", "leadmagnit-callback-open-account");
+              ga("send", "event", "leadmagnit-callback-accoint-in-KZ", "send");
             }
-        },
-        onCountrySelect({ name, iso2, dialCode }) {
-            this.dialCode = dialCode;
-        }
+
+            this.phone = "";
+            this.setSuccess();
+            this.setModal();
+          });
+        // this.phone = "";
+        // this.setSuccess();
+        // this.setModal();
+      }
     },
-    validations: {
-        phone: {
-            required,
-            phoneValidat
-        }
-    }
+    onCountrySelect({ name, iso2, dialCode }) {
+      this.dialCode = dialCode;
+    },
+  },
+  validations: {
+    phone: {
+      required,
+      phoneValidat,
+    },
+  },
 };
 </script>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,6 +16,56 @@
 
     <script src="{{ mix('js/app.js') }}" defer></script>
 
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+  
+  
+    ym(62231704, "init", {
+         clickmap:true,
+         trackLinks:true,
+         accurateTrackBounce:true,
+         webvisor:true,
+         trackHash:true
+    });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/62231704" ; style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162968640-4"></script>;
+    <script>
+        window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+
+
+ gtag('config', 'UA-162968640-4');
+    </script>
+
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1741418499343983');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=1741418499343983&ev=PageView&noscript=1" ; /></noscript>
+    <!-- End Facebook Pixel Code -->
+
+
+
 </body>
 
 </html>
