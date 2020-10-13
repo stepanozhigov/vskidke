34c69b94d5cd325dba0418f9d697989f4d8dde54
stepanozhigov <stepan.ozhigov@gmail.com>
2020-10-13 11:14:47 +0200

    dont open auto

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,5 +20,6 @@ mix.js("resources/js/app.js", "public/js")
     // .copy("resources/images/*", "public/images")
     // .copy("resources/fonts/*", "public/fonts")
     .browserSync({
-        proxy: "lmr.vskidke.local"
+        proxy: "lmr.vskidke.local",
+        open: false
     });
