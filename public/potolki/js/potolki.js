93dffd2700ea64696aee99cf1fb2406807f357a9
Stepan Ozhigov <stepan.ozhigov@gmail.com>
2020-10-17 21:20:45 +0200

    dev

--- public/potolki/js/potolki.js
+++ public/potolki/js/potolki.js
@@ -20623,8 +20623,8 @@ vue__WEBPACK_IMPORTED_MODULE_0___default.a.use(vuex__WEBPACK_IMPORTED_MODULE_1__
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\WebProjects\vskidke\resources\potolki\js\potolki.js */"./resources/potolki/js/potolki.js");
-module.exports = __webpack_require__(/*! C:\WebProjects\vskidke\resources\potolki\scss\app.scss */"./resources/potolki/scss/app.scss");
+__webpack_require__(/*! /Users/stepan/Documents/vskidke/resources/potolki/js/potolki.js */"./resources/potolki/js/potolki.js");
+module.exports = __webpack_require__(/*! /Users/stepan/Documents/vskidke/resources/potolki/scss/app.scss */"./resources/potolki/scss/app.scss");
 
 
 /***/ })
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AppController;
 use App\Http\Controllers\LeadController;
 use Illuminate\Support\Facades\Route;
 
-Route::domain('{subdomain}.vskidke.' . (env('APP_ENV') == 'local' ? 'local' : 'ru'))->group(function () {
+Route::domain('{subdomain}.'.env('APP_URL'))->group(function () {
 
     Route::get('/', [AppController::class, 'index']);
 });
