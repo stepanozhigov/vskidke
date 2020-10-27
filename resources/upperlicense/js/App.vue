ee06444199dcc9e85439310bd98cc83c77b81082
stepanozhigov <stepan.ozhigov@gmail.com>
2020-10-27 16:45:34 +0200

    prod

--- resources/upperlicense/js/App.vue
+++ resources/upperlicense/js/App.vue
@@ -110,7 +110,7 @@
 					this.latitude = location.coords.latitude;
 					this.longitude = location.coords.longitude;
 					const address_data = await axios(this.addressUrl);
-					console.log(address_data);
+					//console.log(address_data);
 					if (address_data.data.items.length > 0)
 						this.setGeoLocation(address_data.data.items[0]);
 				} catch (e) {
