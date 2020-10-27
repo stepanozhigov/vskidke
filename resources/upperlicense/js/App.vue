ae7bb7db0ad86b76e7dcafb574b29e5f0a8cf9f1
stepanozhigov <stepan.ozhigov@gmail.com>
2020-10-27 16:39:33 +0200

    prod

--- resources/upperlicense/js/App.vue
+++ resources/upperlicense/js/App.vue
@@ -98,10 +98,10 @@
 				try {
 					this.gettingLocation = false;
 					const location = await this.getCoords();
+					console.log(location);
 					this.latitude = location.coords.latitude;
 					this.longitude = location.coords.longitude;
 					const address_data = await axios(this.addressUrl);
-					console.log(address_data);
 					if (address_data.data.items.length > 0)
 						this.setGeoLocation(address_data.data.items[0]);
 				} catch (e) {
