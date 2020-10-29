668d4d0e21362ec73c29b7f73c8f8c1b8eb644b4
stepanozhigov <stepan.ozhigov@gmail.com>
2020-10-29 15:22:47 +0200

    prod

--- resources/koronateh/views/layouts/main.blade.php
+++ resources/koronateh/views/layouts/main.blade.php
@@ -25,10 +25,10 @@
     <!-- Global site tag (gtag.js) - Google Analytics -->
 
     <!-- Yandex.Metrika counter -->
-    <script type="text/javascript" >
+    <script type="text/javascript">
         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
         m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js";, "ym");
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
     
         ym(68785867, "init", {
             clickmap:true,
@@ -37,7 +37,10 @@
             webvisor:true
         });
     </script>
-    <noscript><div><img src="https://mc.yandex.ru/watch/68785867"; style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <noscript>
+        <div>
+            <img src="https://mc.yandex.ru/watch/68785867"; style="position:absolute; left:-9999px;" alt="" />
+        </div></noscript>
     <!-- /Yandex.Metrika counter -->
 
 </body>
