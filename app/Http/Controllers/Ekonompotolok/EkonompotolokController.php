29520b5ac4060fea28163c01e54ce223aecf7cc7
stepanozhigov <stepan.ozhigov@gmail.com>
2020-11-24 00:19:40 +0200

    update

--- app/Http/Controllers/Ekonompotolok/EkonompotolokController.php
+++ app/Http/Controllers/Ekonompotolok/EkonompotolokController.php
@@ -29,7 +29,7 @@ class EkonompotolokController extends Controller
                 //СТАТУС
                 'STATUS_ID'=>env('APP_ENV') == 'local' ? 'Хлам' : '',
                 //Лид (стр2)
-                'SOURCE_ID'	=>env('APP_ENV') == 'local' ? 'Тест' : 'Веб-сайт',
+                'SOURCE_ID'	=>env('APP_ENV') == 'local' ? 'Тест' : 'эконом-потолок.рус',
                 //Ответственный (ID)
                 'ASSIGNED_BY_ID'    =>  551,
                 //Город (ID)
