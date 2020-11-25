de750e714d439418daf22123f6397e885079fc12
stepanozhigov <stepan.ozhigov@gmail.com>
2020-11-25 13:01:08 +0200

    route update

--- routes/localhost.php
+++ routes/localhost.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::pattern('localhost', '(localhost)');
 Route::domain('{localhost}')
     ->group(function () {
-        Route::get('/{city?}', function () {
+        Route::get('/{city}', function () {
             return view('app');
            })->where('city', '[\/\w\.-]*');
         Route::post('/bx24', 'EkonompotolokController@bitrix24')->name('bitrix24');
