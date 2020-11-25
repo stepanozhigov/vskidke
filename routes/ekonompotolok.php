5eb7921a76ff22a44e1dd85638ee0d113e60a692
stepanozhigov <stepan.ozhigov@gmail.com>
2020-11-25 12:58:01 +0200

    /city a must in ekonompotolok route

--- routes/ekonompotolok.php
+++ routes/ekonompotolok.php
@@ -6,7 +6,7 @@ Route::pattern('ekonompotolok', '(ekonom-potolok.|www.ekonom-potolok.|www.xn----
 Route::domain('{ekonompotolok}' . (env('APP_ENV') == 'local' ? env('APP_ENV') : env('APP_URL_RUS')))
     ->group(function () {
         // Route::get('/', 'EkonompotolokController@index')->name('home');
-        Route::get('/{city?}', function () {
+        Route::get('/{city}', function () {
             return view('app');
            })->where('city', '[\/\w\.-]*');
         Route::post('/bx24', 'EkonompotolokController@bitrix24')->name('bitrix24');
