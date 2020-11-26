b43b704a2109ca85ff16c85c6ffecf5422a38228
stepanozhigov <stepan.ozhigov@gmail.com>
2020-11-26 11:30:28 +0200

    routes

--- routes/ekonompotolok.php
+++ routes/ekonompotolok.php
@@ -6,7 +6,7 @@ Route::pattern('econom', '(econom.|www.econom.)');
 Route::domain('{econom}' .env('APP_URL'))
     ->group(function () {
         // Route::get('/', 'EkonompotolokController@index')->name('home');
-        Route::get('/{city}', function () {
+        Route::get('/{city?}', function () {
             return view('app');
            })->where('city', '[\/\w\.-]*');
         Route::post('/bx24', 'EkonompotolokController@bitrix24')->name('bitrix24');
