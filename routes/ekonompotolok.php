71170632a1a05a57751943a2829fd799cb8ae9f9
stepanozhigov <stepan.ozhigov@gmail.com>
2020-11-26 11:00:08 +0200

    econom domain name

--- routes/ekonompotolok.php
+++ routes/ekonompotolok.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::pattern('ekonompotolok', '(ekonom-potolok.|www.ekonom-potolok.|www.xn----stbbddfgbcabi4bzk.|xn----stbbddfgbcabi4bzk.)');
-Route::domain('{ekonompotolok}' . (env('APP_ENV') == 'local' ? env('APP_ENV') : env('APP_URL_RUS')))
+Route::pattern('ekonompotolok', '(econom.|www.econom.)');
+Route::domain('{ekonompotolok}' . (env('APP_ENV') == 'local' ? env('APP_ENV') : env('APP_URL')))
     ->group(function () {
         // Route::get('/', 'EkonompotolokController@index')->name('home');
         Route::get('/{city}', function () {
