f93adf6f6a42654021d278c51be98c18cdd280a9
stepanozhigov <stepan.ozhigov@gmail.com>
2020-12-15 12:17:57 +0200

    medlicense google update

--- resources/upperlicense/views/layouts/main.blade.php
+++ resources/upperlicense/views/layouts/main.blade.php
@@ -45,14 +45,35 @@
     <!-- End Facebook Pixel Code -->
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162968640-4"></script>
+    <script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-92968269-7"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+    
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+    
         gtag('js', new Date());
-        gtag('config', 'UA-162968640-4');
+    
+        gtag('config', 'UA-92968269-7');
     </script>
     <!-- Global site tag (gtag.js) - Google Analytics -->
+
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+        ga('create', 'UA-92968269-7', 'auto');
+        ga('send', 'pageview');
+    </script>
 
     <!-- Yandex.Metrika counter -->
     <script type="text/javascript" >
