462d3bccb1b4d8c3d6234826ab856746df90941f
stepanozhigov <stepan.ozhigov@gmail.com>
2020-12-18 10:39:25 +0200

    bg

--- resources/redzoloto/views/app.blade.php
+++ resources/redzoloto/views/app.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('title')
-    Салон красоты MG
+    Redzoloto
 @endsection
 
 @section('pixel')
@@ -15,11 +15,11 @@
   t.src=v;s=b.getElementsByTagName(e)[0];
   s.parentNode.insertBefore(t,s)}(window, document,'script',
   'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '1504770689730535');
+  fbq('init', '811845732943898');
   fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=1504770689730535&ev=PageView&noscript=1"
+  src="https://www.facebook.com/tr?id=811845732943898&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Facebook Pixel Code -->
 @endsection
