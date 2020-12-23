a45e45e82cfd2df793a33fe15c2eb77fc01b3cfc
stepanozhigov <stepan.ozhigov@gmail.com>
2020-12-23 17:04:47 +0200

    zoloto prod

--- app/Http/Controllers/Redzoloto/RedzolotoController.php
+++ app/Http/Controllers/Redzoloto/RedzolotoController.php
@@ -18,21 +18,22 @@ class RedzolotoController extends Controller
         return view('app');
     }
 
-    public function mail(Request $request)
-    {
-        $to = env('APP_ENV') == 'local' ? env('MAIL_TO_TEST') : 'spa@mosgym.ru';
-        Mail::to($to)->send(new SendMgMail($request->title,$request->phone));
-    }
-
     public function bitrix24(Request $request)
     {
+
+        //redzoloto
+        //$response = Http::post('https://redzoloto.bitrix24.ru/rest/6/x04kestbu8ul91qc/crm.lead.add.json',$data);
+        //ours
+        //$webhook = 'https://bitrix-ts.ru/rest/578/xq4ts7jyl4q74hed/';
+        $webhook = 'https://redzoloto.bitrix24.ru/rest/6/x04kestbu8ul91qc/';
+
         $utm = $request->input('utm');
         $data = [
             'fields'=>[
                 //Лид (стр1)
                 'TITLE'=> $request->input('title'),
                 //СТАТУС
-                'STATUS_ID'=>env('APP_ENV') == 'local' ? 'Хлам' : '',
+                'STATUS_ID'=>env('APP_ENV') == 'local' ? 'JUNK' : '',
                 //Лид (стр2)
                 //'SOURCE_ID'	=>$request->input('utm_source') ?? env('APP_ENV') == 'local' ? 'Тест' : 'Веб-сайт',
                 'SOURCE_ID'	=>'WEB',
@@ -49,6 +50,9 @@ class RedzolotoController extends Controller
                 //referer (источник)
                 'UF_CRM_1608543475'=>$request->input('referer') ?? '',
 
+                //CONTACT
+                'CONTACT_ID'=>'',
+
                 //UTM
                 'UTM_CAMPAIGN'=>$utm['utm_campaign'] ?? '',
                 'UTM_SOURCE'=>$utm['utm_source'] ?? '',
@@ -58,8 +62,35 @@ class RedzolotoController extends Controller
             ]
         ];
 
-        $response = Http::post('https://redzoloto.bitrix24.ru/rest/6/x04kestbu8ul91qc/crm.lead.add.json',$data);
-        return response()->json($response->json());
-        //return response()->json($data['fields']);
+        //create with contact 482664
+
+        //get contact 482664
+        $contact_response = Http::post($webhook.'crm.contact.list.json',[
+            'filter' => ['PHONE'=>$request->input('phone')] 
+        ]);
+
+        if($contact_response->successful()) {
+            //create lead with existing contact
+            if(count($contact_response->json()['result'])>0) {
+                $contact = $contact_response->json()['result'][0];
+                $contact_id = intval($contact['ID']);
+                $data['CONTACT_ID'] = $contact_id;
+                $create_response = Http::post($webhook.'crm.lead.add.json',$data);
+            }
+            else {
+                 //create lead with new client
+                $create_response = Http::post($webhook.'crm.lead.add.json',$data);
+             }
+
+             //response
+            if($create_response->successful()) {
+                return response()->json($create_response->json(),201);
+            } else {
+                return response()->json(['error'=>'Повторите заявку'], 400);
+            }
+
+        } else {
+            return response()->json(['error'=>'Повторите заявку'], 400);
+        }
     }
 }
